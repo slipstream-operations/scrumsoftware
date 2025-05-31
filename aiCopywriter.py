8c930e28aa70fcc4293d75dadfcfad51d33ae675
slipstream-operations <jp.techstaging@gmail.com>
2025-05-31 13:09:58 +0100

    Updated UI. Added html generation capability. Added branding. Fixed issue where selection of new options removed the previously generated copy

--- aiCopywriter.py
+++ aiCopywriter.py
@@ -5,13 +5,16 @@ import streamlit as st
 client = OpenAI(api_key=st.secrets["openai_api_key"])
 authcode = st.secrets["authcode"]
 
+st.image("Images/logo.jpeg", width=100)
+
 st.subheader("MPG - Email Helper")
-st.markdown("Instructions: Select an email reference from the bank - Provide context (objective) - Set the tone - Generate")
+st.markdown("**Instructions:** Select an email reference from the bank - Provide context (objective) - Set the tone - Generate")
 
 sheet_url = st.secrets["sheet_one_url"] #reference sheet file
+platformlist = ['Adestra','Hubspot','Pardot','Sailthru']
 
 df = pd.read_csv(sheet_url)
-st.markdown('Email Bank Preview: [Update](https://docs.google.com/spreadsheets/d/1Ojp-0Gm-8-CKC2QdqbWOMjkCFIqhppx3nxQBeMSGYTQ/edit?gid=0#gid=0)')
+st.markdown('Email Bank Preview: [Update the email bank here.](https://docs.google.com/spreadsheets/d/1Ojp-0Gm-8-CKC2QdqbWOMjkCFIqhppx3nxQBeMSGYTQ/edit?gid=0#gid=0)')
 st.dataframe(df, height=150)
 
 reference_list = df['Email_Number'].tolist()
@@ -25,7 +28,7 @@ tone=st.text_input("Enter the general tone (Optional):")
 default_tone = 'Match Refererence'
 
 reference_emailselection = st.selectbox(
-    "Reference Email Number",
+    "Reference Email Number / Identifier:",
     reference_list
     )
 
@@ -53,4 +56,36 @@ if st.button("Generate Email"):
         temperature=0.7,
     )
     output = response.choices[0].message.content
-    st.text_area("Output", output, height=500)
+    st.session_state.email_output_stored = output #saved the output in session state
+
+if 'email_output_stored' in st.session_state:
+     st.text_area("Result:", st.session_state.email_output_stored, height=500)
+     st.session_state.output_state = 1 #to signal an output by the AI/LLM has been generated at least once
+
+
+#Area loaded only after initial email is generated
+if 'output_state' in st.session_state and int(st.session_state.output_state)>0:
+   st.write('')
+   st.write('Construct HTML version (Optional):')
+   selected_platform = st.selectbox("Select Platform:",
+                platformlist)
+   
+   html_prompt = 'Generate a html version of the provided email: \n\n' + st.session_state.email_output_stored + '\n\n to set it up in this email platform: ' + selected_platform
+
+   if st.button("Generate HTML Version"):
+      response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": "You are an event marketer and are given email copy to setup in a mentioned email platform. To do this you need to convert the email to a html version, compatible with the email platform provided to you."},
+                   {"role": "user", "content": html_prompt}],
+        temperature=0.7,
+    )
+      st.session_state.html_output = response.choices[0].message.content
+   
+
+if 'html_output' in st.session_state:
+     st.text_area("Result:", st.session_state.html_output, height=500)
+   
+
+
+
+
